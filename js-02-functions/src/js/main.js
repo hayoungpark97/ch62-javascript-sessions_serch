f2e61bfe1b89e18b488248112a5db6ee45c0dc9a
Hayoung Park <hayoungpark97@gmail.com>
2025-11-27 13:09:51 -0600

    feat: cambios de funciones

--- js-02-functions/src/js/main.js
+++
@@ -1,6 +0,0 @@
-console.log( saludar ("Hanna")); // undefined porque la funcion saludar() no retorna nada
-
-
-function saludar (nombre) {
-    console.log( `Hola, ${nombre},que vas a comer hoy?`);
-}
